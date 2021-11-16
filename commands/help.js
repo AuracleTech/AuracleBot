9dbf9c0edafa1f7d828f742188c9b79857bf1d81
Auracle <virgilfabre@outlook.com>
2021-11-16 11:25:50 -0500

    Code Cleanup

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,2 @@
 module.exports.help = { name: 'help', alias: 'h' }
 module.exports.run = async (message, args, callback) => { callback('[https://github.com/AuracleTech/AuracleTech-Osu see the GitHub page]') }
-
