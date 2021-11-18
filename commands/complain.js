d91d1672ec1b4cf333f4234210e75b4c87d36747
Auracle <virgilfabre@outlook.com>
2021-11-18 00:39:04 -0500

    Complain command completed

--- commands/complain.js
+++ commands/complain.js
@@ -1,2 +1,8 @@
+// Importing Modules
+const log = require('../utils').log
+
 module.exports.help = { name: 'complain', alias: 'c' }
-module.exports.run = async (instance, args, callback) => { callback('NOT IMPLEMENTED FOR VERSION 2!') }
+module.exports.run = async (instance, args, callback) => {
+    log(`${instance.user.ircUsername} complained : ${args.join(' ')}`, 3)
+	callback(`Your message is now displayed for the owner to see.`)
+}
