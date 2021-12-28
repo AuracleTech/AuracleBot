5f11b30f409b8b3dc9b87508961cee7b77c5b9a3
Auracle <virgilfabre@outlook.com>
2021-12-28 14:44:23 -0500

    Adding performance monitoring
    Fixes #12

--- performanceManager.js
+++ performanceManager.js
@@ -3,6 +3,8 @@ const beatmapsManager = require('./beatmapsManager')
 const enum_mods = require('./enums/mods')
 const log = require('./utils').log
 const execFile = require('child_process').execFile
+const { performance } = require('perf_hooks')
+const fs = require('fs')
 
 module.exports.calculatePerformance = async (mapID, gamemode, mods = []) => {
 	let filename = new Date().getTime()
@@ -10,19 +12,22 @@ module.exports.calculatePerformance = async (mapID, gamemode, mods = []) => {
 	if (gamemode == 'mania') accs = [100]
 	await beatmapsManager.downloadBeatmap(mapID, filename)
 	let results = []
-	for (let acc of accs)
-		results.push(await launchPerformanceCalculator(filename, acc, gamemode, mods))
-	return results
+	for (let acc of accs) results.push(launchPerformanceCalculator(filename, acc, gamemode, mods))
+	let performances = await Promise.all(results)
+	fs.unlinkSync(`${process.env.FOLDER_TEMP}${filename}.osu`)
+	return performances
 }
 
 launchPerformanceCalculator = async (filename, acc, gamemode, mods = []) => {
+var startTime = performance.now()
 	return new Promise((resolve, reject) => {
 		let args = [ 'simulate', gamemode, '-a', acc, `${process.env.FOLDER_TEMP}${filename}.osu`, '-j' ]
 		for (let mod of mods) args.push('-m', enum_mods.getAbbreviation(mod).toLowerCase())
 	    execFile('./osu-tools-master/PerformanceCalculator/bin/Release/net5.0/PerformanceCalculator.exe', args, (err, stdout, stderr) => {
-			fs.unlinkSync(`${index.tempFolder}${filename}.osu`)
+var endTime = performance.now()
+log(`PerformanceCalculator took ${(endTime - startTime).toFixed()} milliseconds`, 4)
 			if (err) {
-				log(`${this.filename}.launchPerformanceCalculator ${stderr}`, 3)
+				log(`${filename}.launchPerformanceCalculator ${stderr}`, 3)
 				return reject(stderr)
 			}
 			return resolve(JSON.parse(stdout))
--- scoresManager.js
+++ scoresManager.js
@@ -3,12 +3,16 @@ const node_osu = require('node-osu')
 let api_osu = new node_osu.Api(process.env.API_KEY, { notFoundAsError: false, completeScores: true })
 const db = require('./db')
 const index = require('./index')
+const { performance } = require('perf_hooks')
 
 exports.getTopScores = async (user) => {
+var startTime = performance.now()
     db.upsert(index.db_settings, user.id, { lastScoreGrabDate: new Date() })
     api_osu.getUserBest({ u: user.id, type: "id" }).then(async scores => {
         for (let score of scores) await db.upsert(index.db_scores, `${score.user.id}${score.beatmapId}`, { score })
     })
+var endTime = performance.now()
+console.log(`getTopScores took ${(endTime - startTime).toFixed()} milliseconds`)
 }
 
 exports.isOnCooldown = async (user) => {
